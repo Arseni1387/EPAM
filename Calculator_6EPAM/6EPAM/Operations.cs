219b0c0086b9e1eeb2ef8bc8c73b0813aed35e7f
Arseni Kravzhul <Kravzhul2002@mail.ru>
2021-11-22 00:03:32 +0300

    ADD

---
+++ Calculator_6EPAM/6EPAM/Operations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6EPAM
+{
+    public class Operations
+    {
+        static public int Plus(int firstTerm, int secondTerm)
+        {
+            if ((double)firstTerm + (double)secondTerm > int.MaxValue)
+            {
+                throw new ArgumentException("sum is bigger than MaxVaue");
+            }
+
+            if ((double)firstTerm + (double)secondTerm < int.MinValue)
+            {
+                throw new ArgumentException("sum is less than MinVaue");
+            }
+
+            return firstTerm + secondTerm;
+        }
+
+        static public int Minus(int firstTerm, int secondTerm)
+        {
+            return firstTerm - secondTerm;
+        }
+
+        static public int Multiply(int firstTerm, int secondTerm)
+        {
+            return firstTerm * secondTerm;
+        }
+
+        static public int Division(int firstTerm, int secondTerm)
+        {
+            if (secondTerm == 0)
+            {
+                throw new ArgumentException(nameof(secondTerm));
+            }
+
+            return firstTerm / secondTerm;
+        }
+    }
+}
---
+++ Calculator_6EPAM/6EPAM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _6EPAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
